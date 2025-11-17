b43eab47689fa9e7f8a38603abbd47872030652a
Dylan Austin <austindylan0@gmail.com>
2025-11-17 03:18:33 +0000

    Hello, world?

---
+++ hello-world/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>   // include information about standard input/output library
+ int main()
+ {
+    printf("hello, world\n");   // print hello, world followed by a new line
+    return 0;
+ }
+
+// a function called main that received no argument values statements of main are enclosed in braces 
+// main calls library function printf
+// to print this sequence of characters
+// \n represents the newline character
